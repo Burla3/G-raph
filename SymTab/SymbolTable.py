ecb33f0c17878c793edc3864e184330bb3689178
Mathias Nielsen <lynge@lynges.net>
2017-04-07 14:03:57 +0200

    symbol table added

---
+++ SymTab/SymbolTable.py
@@ -0,0 +1,34 @@
+
+class SymbolTable():
+    symbols = {}
+
+    def get(self, symkey):
+        if self.symbols[symkey].type == 'ref':
+            return self.symbols[symkey].value[self.symbols[symkey].value.refkey]
+        else:
+            return self.symbols[symkey]
+
+    def _create_sym(self, symkey, symtype, value):
+        self.symbols[symkey] = {'type': symtype, 'value': value}
+
+    def _set_sym(self, symkey, symtype, value):
+        self.symbols[symkey].type = symtype
+        self.symbols[symkey].value = value
+
+    def _set_sym_ref(self, symkey, value):
+        self.symbols[symkey].value.reftab[self.symbols[symkey].value.refkey] = value
+
+    def set(self, symkey, symtype, value, reftab=None, refkey=None):
+        if symtype == 'ref':
+            if not reftab or not refkey:
+                raise ValueError(
+                    'When setting a symbol reference type, you must supply a table(reftab) and key(refkey)')
+            value = {'table': reftab, 'key': refkey}
+
+        if symkey in self.symbols:
+            if symtype == 'ref':
+                self._set_sym_ref(symkey, value)
+            else:
+                self._set_sym(symkey, symtype, value)
+        else:
+            self._create_sym(symkey, symkey, value)
