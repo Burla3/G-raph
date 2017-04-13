28df308b654a71e583e6ba381836210429878024
Jens Burla Johansen <jens@burla.dk>
2017-04-13 13:47:08 +0200

    Created TemplateBuilder and added all generated files.

---
+++ CodeGeneration/CodeGenerator.py
@@ -0,0 +1,104 @@
+class CodeGenerator:
+
+    def generateProgram(self):
+        pass
+
+    def generateProcDef(self):
+        pass
+
+    def generateFormalParams(self):
+        pass
+
+    def generateBlock(self):
+        pass
+
+    def generateStmt(self):
+        pass
+
+    def generateSimpleStmt(self):
+        pass
+
+    def generateAssignment(self):
+        pass
+
+    def generatePrintStmt(self):
+        pass
+
+    def generateLengthStmt(self):
+        pass
+
+    def generateCompoundStmt(self):
+        pass
+
+    def generateIfStmt(self):
+        pass
+
+    def generateWhileStmt(self):
+        pass
+
+    def generateForeachStmt(self):
+        pass
+
+    def generateGraphAssignment(self):
+        pass
+
+    def generateTest(self):
+        pass
+
+    def generateCompOp(self):
+        pass
+
+    def generateExpr(self):
+        pass
+
+    def generateSetOp(self):
+        pass
+
+    def generateFactorOp(self):
+        pass
+
+    def generateMolecule(self):
+        pass
+
+    def generateAtom(self):
+        pass
+
+    def generateTrailer(self):
+        pass
+
+    def generateFuncCall(self):
+        pass
+
+    def generateGraphKeyword(self):
+        pass
+
+    def generateActualParams(self):
+        pass
+
+    def generateListStruct(self):
+        pass
+
+    def generateGraph(self):
+        pass
+
+    def generateVertices(self):
+        pass
+
+    def generateVertex(self):
+        pass
+
+    def generateEdges(self):
+        pass
+
+    def generateEdge(self):
+        pass
+
+    def generateIdentifier(self):
+        pass
+
+    def generateNumber(self):
+        pass
+
+    def generateBoolean(self):
+        pass
+
---
+++ ContextualAnalysis/TypeChecker.py
@@ -0,0 +1,104 @@
+class TypeChecker:
+
+    def checkProgram(self):
+        pass
+
+    def checkProcDef(self):
+        pass
+
+    def checkFormalParams(self):
+        pass
+
+    def checkBlock(self):
+        pass
+
+    def checkStmt(self):
+        pass
+
+    def checkSimpleStmt(self):
+        pass
+
+    def checkAssignment(self):
+        pass
+
+    def checkPrintStmt(self):
+        pass
+
+    def checkLengthStmt(self):
+        pass
+
+    def checkCompoundStmt(self):
+        pass
+
+    def checkIfStmt(self):
+        pass
+
+    def checkWhileStmt(self):
+        pass
+
+    def checkForeachStmt(self):
+        pass
+
+    def checkGraphAssignment(self):
+        pass
+
+    def checkTest(self):
+        pass
+
+    def checkCompOp(self):
+        pass
+
+    def checkExpr(self):
+        pass
+
+    def checkSetOp(self):
+        pass
+
+    def checkFactorOp(self):
+        pass
+
+    def checkMolecule(self):
+        pass
+
+    def checkAtom(self):
+        pass
+
+    def checkTrailer(self):
+        pass
+
+    def checkFuncCall(self):
+        pass
+
+    def checkGraphKeyword(self):
+        pass
+
+    def checkActualParams(self):
+        pass
+
+    def checkListStruct(self):
+        pass
+
+    def checkGraph(self):
+        pass
+
+    def checkVertices(self):
+        pass
+
+    def checkVertex(self):
+        pass
+
+    def checkEdges(self):
+        pass
+
+    def checkEdge(self):
+        pass
+
+    def checkIdentifier(self):
+        pass
+
+    def checkNumber(self):
+        pass
+
+    def checkBoolean(self):
+        pass
+
