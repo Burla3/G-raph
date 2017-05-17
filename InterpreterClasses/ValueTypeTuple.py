e06006a768ab344c819191bc1573806afe7e69fc
Jalf- <benjaminboendergaard@hotmail.com>
2017-05-17 14:24:20 +0200

    Implemented print for list and graph.

--- InterpreterClasses/ValueTypeTuple.py
+++ InterpreterClasses/ValueTypeTuple.py
@@ -1,4 +1,21 @@
+from InterpreterClasses.Types import Types
+
+
 class ValueTypeTuple():
     def __init__(self, value, type):
         self.value = value
         self.type = type
+
+    def __str__(self):
+        if self.type == Types.List:
+            buildStr = '['
+            index = 0
+
+            while index < len(self.value) - 1:
+                buildStr += str(self.value[index]) + ', '
+                index += 1
+
+            buildStr += str(self.value[index]) + ']'
+            return buildStr
+
+        return str(self.value)
